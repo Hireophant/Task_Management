065c3ee0eecb592a76d16caf256f32bf27e752ef
Hireophant <tntdanh2506@gmail.com>
2026-03-01 05:02:37 +0700

    change status of task

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const database = require("./config/database");
+const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
@@ -7,6 +8,9 @@ const port = process.env.PORT;
 const routesApiVer1 = require("./api/v1/routes/index.route");
 
 database.connect();
+
+// parse application/json
+app.use(bodyParser.json());
 
 // Router Version 1
 routesApiVer1(app);
