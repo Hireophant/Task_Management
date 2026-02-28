728cdf87403337acca8db01cae22d5fb08f1b676
Hireophant <tntdanh2506@gmail.com>
2026-03-01 05:41:07 +0700

    edit task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -116,3 +116,21 @@ module.exports.create = async (req, res) => {
     });
   }
 };
+
+// [PATCH] /api/v1/tasks/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.updateOne({ _id: id }, req.body);
+    res.json({
+      code: 200,
+      message: "Cập nhật công việc thành công",
+      data: task,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Cập nhật công việc thất bại",
+    });
+  }
+};
