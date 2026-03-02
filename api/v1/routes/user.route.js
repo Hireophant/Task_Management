e444928754394bcad4910efcdb382be0f5d31a3b
Hireophant <tntdanh2506@gmail.com>
2026-03-02 08:43:10 +0700

    login feature

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../../../controllers/user.controller");
 
 router.get("/register", controller.register);
+router.post("/login", controller.login);
 
 module.exports = router;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,13 +21,52 @@ module.exports.register = async (req, res) => {
     });
     res.json({
       code: 200,
-      message: "Thêm mới công việc thành công",
+      message: "Tạo tài khoản thành công",
       token: token,
     });
   } catch (error) {
     res.json({
       code: 400,
-      message: "Thêm mới công việc thất bại",
+      message: "Tạo tài khoản thất bại",
+    });
+  }
+};
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+  try {
+    const existingUser = await User.findOne({
+      email: req.body.email,
+      deleted: false,
+    });
+    if (!existingUser) {
+      res.json({
+        code: 400,
+        message: "Email không tồn tại",
+      });
+      return;
+    }
+    req.body.password = md5(req.body.password);
+    if (existingUser.password !== req.body.password) {
+      res.json({
+        code: 400,
+        message: "Sai mật khẩu",
+      });
+      return;
+    }
+    const token = existingUser.token;
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    });
+    res.json({
+      code: 200,
+      message: "Đăng nhập thành công",
+      token: token,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Đăng nhập thất bại",
     });
   }
 };
